28daa0b08215e053e033862253b33f5665c702df
melcd238 <melcd238@gmail.com>
2022-05-18 22:12:50 +0200

    Add polygone and text to Radar Chart

--- src/Components/RadarChart/RadarChart.js
+++ src/Components/RadarChart/RadarChart.js
@@ -29,12 +29,13 @@ const RadarChart = ({datas})=>{
  
  const DrawRadarChart = (elt , data)=>{
               //Variables usefull:
-              const margin = {top:20,right:14,bottom:20, left:14}
-              const width = 258 - margin.left - margin.right
-              const height = 253 - margin.top - margin.bottom
+              const margin = {top:20,right:0,bottom:20, left:0}
+              const width = 258 
+              const height = 253
               const buffer = 35
               const radius = width /2 - buffer
-            
+              const maxValue = d3.max(data, function(d){return d.value})
+             
     
        d3.select(elt).select("svg").remove() // remove odl svg
        
@@ -47,29 +48,79 @@ const RadarChart = ({datas})=>{
            .attr("width", width + margin.left + margin.right)
            svg.append("g")
            .attr('id','radialChart')
-           .attr('transform', 'translate(' + margin.left + "," + margin.top + ")")
-           //.attr('transform', `translate(${width / 2}, ${width / 2})`);
+           .attr('transform', `translate(${width / 2}, ${width / 2})`)
            
-     /*      const radialScale = d3.scaleLinear()
-                               .domain([0 ,d3.max(data, function(d){return d.value})])
+           
+          const radialScale = d3.scaleLinear()
+                               .domain([0 ,maxValue +10])
                                .range([0 , radius])
            const radialLine = d3.lineRadial()
                               .angle(d => d[0] * (Math.PI / 180))
                               .radius(d => radialScale(d[1])) 
                               
            const Angle = 360 / data.length; 
-           
+
+             
+          
+           // Add Axis from center 
            data.forEach((spend, index)=>{
              svg.select('#radialChart')
                  .append('path')
                  .attr('id', `axisPerform${index}`)
                  .attr('d', radialLine([
-                   [index * Angle, 0], [index * Angle, d3.max(data, function(d){return d.value}) ]
+                   [index * Angle, 0], [index * Angle,maxValue ]
                  ]))
                  .style('fill', "none")
                  .style("stroke-width", '0,5')
-                 .style("stroke", 'white')
-           }) */
+                 .style("stroke", 'hsla(203, 9%, 17%, 1)')
+                
+  
+                    // Add Text from axis 
+           const Path = radialLine([
+            [index * Angle, 0], [index * Angle,maxValue ]
+          ])
+            const selectIndex = Path.indexOf('L')
+            const selectPosition = Path.slice(selectIndex + 1)
+            let [X,Y] = [...selectPosition.split(',')]
+            svg.select('#radialChart')
+               .append('text')
+               .attr('class','axisPerformtext')
+               .text(()=>spend.kind)
+               .attr('x', X)
+               .attr('y', Y)
+               .style('text-anchor','middle')
+               .style('fill','black')
+               .style('font-size', '8px')
+               .transition()
+               .duration(2000)
+               .style('fill', 'white')
+
+           }) 
+
+           // create Polygone
+           function createPolygon(rad){
+            svg.select('#radialChart')
+            .append('g')
+            .attr('class','poly')
+            .selectAll('path')
+            .data([data])
+            .join('path')
+            .attr('d', d3.lineRadial()
+             .angle((d,i)=>(i* Angle) * (Math.PI / 180))
+             .radius(()=>radialScale(maxValue/rad)).curve(d3.curveLinearClosed))
+            .style('fill','none')
+            .style('stroke', "white")
+            .attr('stroke-width', 1)
+            }
+            createPolygon(1.2)
+            createPolygon(1.6)
+            createPolygon(2)
+            createPolygon(3)
+            createPolygon(5.5)
+
+            // ADD RadialLine of values. 
+       
+     
 
     }
 
