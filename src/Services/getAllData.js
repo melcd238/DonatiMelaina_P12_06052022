55ca2f395e3b2b4fbb83efdfc572b671e9b7a813
melcd238 <melcd238@gmail.com>
2022-05-30 14:31:52 +0200

    update getAllData

--- src/Services/getAllData.js
+++ src/Services/getAllData.js
@@ -1,22 +1,22 @@
+
 import { getUserMainData, getUserActivity, getUserAverageSessions, getUserPerformance  } from "./CallAPI"
 //import { getUserMainDataMocked, getUserActivityMocked, getUserAverageSessionsMocked, getUserPerformanceMocked } from './CallMockedData';
 
-
+// To use Mocked Data, switch the import from CallMockedData and the variables in getAllData 
 
 export const getAllData = async (id)=>{
-    const user =  await getUserMainData(id);
-    const average = await getUserAverageSessions(id);
-    const activity = await getUserActivity(id);
-    const perform = await getUserPerformance(id);
-   return Promise.all([user, average, activity,perform])
+   
+       const user =  await getUserMainData(id);
+       const average = await getUserAverageSessions(id);
+       const activity = await getUserActivity(id);
+       const perform = await getUserPerformance(id);
+       /* const user =  await getUserMainDataMocked(id);
+        const average = await getUserAverageSessionsMocked(id);
+        const activity = await getUserActivityMocked(id);
+        const perform = await getUserPerformanceMocked(id);*/
+      return Promise.all([user, average, activity,perform])
+    
 }
 
 
 
-// If you want to use Mocked data, use the import from CallMockedData and the following variables 
-
-/*      const user =  await getUserMainDataMocked(id);
-        const average = await getUserAverageSessionsMocked(id);
-        const activity = await getUserActivityMocked(id);
-        const perform = await getUserPerformanceMocked(id);
-        Promise.all([user, average, activity,perform]) */
